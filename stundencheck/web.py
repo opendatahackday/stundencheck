d6c6b1d8c7c728b29ae4ab8d522c542b48349f02
Friedrich Lindenberg <friedrich@pudo.org>
2012-01-16 19:49:41 +0100

    rough straw man

--- stundencheck/web.py
+++ stundencheck/web.py
@@ -2,7 +2,7 @@
 from collections import defaultdict
 from datetime import datetime
 
-from colander import Invalid
+import colander
 from flask import Flask, g, request, render_template, abort, flash, json
 from flask import url_for, redirect, jsonify
 from flask.ext import admin
@@ -14,6 +14,26 @@ from stundencheck.model import Event, Report, School
 admin_datastore = SQLAlchemyDatastore((School, Event, Report), db.session)
 admin_blueprint = admin.create_admin_blueprint(admin_datastore)
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+
+class SchoolType(colander.SchemaType):
+
+    def serialize(self, node, struct):
+        return struct.id
+
+    def deserialize(self, node, struct):
+        return db.session.query(School).filter_by(id=struct).first()
+
+
+class ReportSchema(colander.MappingSchema):
+    school = colander.SchemaNode(SchoolType())
+    group = colander.SchemaNode(colander.String())
+    course = colander.SchemaNode(colander.String())
+    session = colander.SchemaNode(colander.String())
+    reason = colander.SchemaNode(colander.String())
+    email = colander.SchemaNode(colander.String())
+
+
 
 @app.template_filter()
 def datetime_format(dt):
@@ -31,12 +51,43 @@ def school(id):
         abort(404)
     return render_template('school.tmpl', school=school)
 
+@app.route("/report")
+def report():
+    school_id = request.args.get('school')
+    schools = db.session.query(School)
+    return render_template('report.tmpl', schools=schools,
+            school_id=school_id)
 
+@app.route("/report", methods=['POST'])
+def report_save():
+    try:
+        data = dict(request.form.items())
+        data = ReportSchema().deserialize(data)
+        # TODO: fuzzy match events.
+        event = Event()
+        event.school = data['school']
+        event.group = data['group']
+        event.course = data['course']
+        event.session = data['session']
+        event.reason = data['reason']
+        db.session.add(event)
+        report_ = Report()
+        report_.event = event
+        report_.email = data['email']
+        db.session.add(report_)
+        db.session.commit()
+        return redirect(url_for('school', 
+            id=event.school.id))
+    except colander.Invalid, i:
+        #print i
+        return report()
 
 @app.route("/")
 def index():
     schools = db.session.query(School)
-    return render_template('index.tmpl', schools=schools)
+    count = db.session.query(Report).count()
+    return render_template('index.tmpl', schools=schools, 
+            count=count)
 
 if __name__ == '__main__':
     db.create_all()
